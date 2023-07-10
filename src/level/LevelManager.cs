9705117e396f95e169a5f5d4bb67bb0e50ec8227
xenological <98416161+xeno-logical@users.noreply.github.com>
2023-07-09 23:35:18 -0400

    Added Models

--- src/level/LevelManager.cs
+++ src/level/LevelManager.cs
@@ -3,19 +3,19 @@ using Godot;
 
 public class LevelManager : Node
 {
-    private GameManager _gameManager;
+	private GameManager _gameManager;
 
-    public static LevelManager MustGetNode(Node self) {
-        return self.GetNode<LevelManager>("/root/Level") ?? throw new System.NullReferenceException();
-    }
+	public static LevelManager MustGetNode(Node self) {
+		return self.GetNode<LevelManager>("/root/Level") ?? throw new System.NullReferenceException();
+	}
 
-    [Export]
-    private NodePath arHolderPath;
-    public ARHolderControl ARHolder {get; private set;}
+	[Export]
+	private NodePath arHolderPath;
+	public ARHolderControl ARHolder {get; private set;}
 
-    public override void _Ready()
-    {
-        _gameManager = GetNode<GameManager>("/root/GameManager") ?? throw new System.NullReferenceException();
-        ARHolder = GetNode<ARHolderControl>(arHolderPath) ?? throw new NullReferenceException();
-    }
+	public override void _Ready()
+	{
+		_gameManager = GetNode<GameManager>("/root/GameManager") ?? throw new System.NullReferenceException();
+		ARHolder = GetNode<ARHolderControl>(arHolderPath) ?? throw new NullReferenceException();
+	}
 }
