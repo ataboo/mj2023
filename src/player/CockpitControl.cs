45d8172b89144213b7d4bd1af30592f466871737
xenological <98416161+xeno-logical@users.noreply.github.com>
2023-07-15 10:41:30 -0400

    Updated from main

--- src/player/CockpitControl.cs
+++ src/player/CockpitControl.cs
@@ -3,51 +3,64 @@ using System;
 
 public class CockpitControl : Spatial
 {
-	[Export]
-	public NodePath playerControlPath;
-	private PlayerControl _playerControl;
+    [Export]
+    public NodePath playerControlPath;
+    private PlayerControl _playerControl;
 
-	[Export]
-	public NodePath cameraPath;
-	private Camera _camera;
+    [Export]
+    public NodePath cameraPath;
+    private Camera _camera;
 
-	private HudControl _hudControl;
+    private HudControl _hudControl;
 
-	private GameManager _gameManager;
+    private GameManager _gameManager;
 
-	private LevelManager _levelManager;
+    private LevelManager _levelManager;
 
-	[Export]
-	float walkBobScale = 0.2f;
+    [Export]
+    float walkBobScale = 0.2f;
 
-	private Vector3 _startPos;
+    private Vector3 _startPos;
 
-	public override void _Ready()
-	{
-		_gameManager = GameManager.MustGetNode(this);
-		this._startPos = this.Translation;
-		this._playerControl = GetNode<PlayerControl>(playerControlPath) ?? throw new NullReferenceException();
-		this._camera = GetNode<Camera>(cameraPath) ?? throw new NullReferenceException();
-		
-		_levelManager = LevelManager.MustGetNode(this) ?? throw new NullReferenceException();
-		this._hudControl = _levelManager.HudControl ?? throw new NullReferenceException();
+    [Export]
+    private NodePath raycastPath;
+    private RayCast _camRaycast;
 
-		_playerControl.Connect(nameof(PlayerControl.OnPhysicsDone), this, nameof(_HandleChassisPhysicsComplete));
-	}
+    public override void _Ready()
+    {
+        _gameManager = GameManager.MustGetNode(this);
+        this._startPos = this.Translation;
+        this._playerControl = GetNode<PlayerControl>(playerControlPath) ?? throw new NullReferenceException();
+        this._camera = GetNode<Camera>(cameraPath) ?? throw new NullReferenceException();
+        _camRaycast = GetNode<RayCast>(raycastPath) ?? throw new NullReferenceException();
+        
+        _levelManager = LevelManager.MustGetNode(this) ?? throw new NullReferenceException();
+        this._hudControl = _levelManager.HudControl ?? throw new NullReferenceException();
 
-	public void _HandleChassisPhysicsComplete(float delta) {
-		if(!_gameManager.UserConfig.headBobActive) {
-			Translation = Vector3.Zero;
-			return;
-		}
-		var mechState = _playerControl.MechState;
-		var bobScale = mechState.shuffling ? walkBobScale / 3 : walkBobScale;
-		var targetPos = new Vector3(Mathf.Cos(mechState.walkCycleT * Mathf.Pi*2) * bobScale, Mathf.Cos(mechState.walkCycleT * Mathf.Pi * 4) * bobScale, 0) + _startPos;
+        _playerControl.Connect(nameof(PlayerControl.OnPhysicsDone), this, nameof(_HandleChassisPhysicsComplete));
+        _camRaycast.CastTo = new Vector3(0, 0, -300);
+    }
 
-		Translation = Translation.LinearInterpolate(targetPos, delta * 10f);
-				  
-		Rotation = new Vector3(0, mechState.lookTarget.x, 0);
-		RotateObjectLocal(Vector3.Right, mechState.lookTarget.y);
-	}
+    public void _HandleChassisPhysicsComplete(float delta) {
+        if(!_gameManager.UserConfig.headBobActive) {
+            Translation = Vector3.Zero;
+            return;
+        }
+        var mechState = _playerControl.MechState;
+        var bobScale = mechState.shuffling ? walkBobScale / 3 : walkBobScale;
+        var targetPos = new Vector3(Mathf.Cos(mechState.walkCycleT * Mathf.Pi*2) * bobScale, Mathf.Cos(mechState.walkCycleT * Mathf.Pi * 4) * bobScale, 0) + _startPos;
 
+        Translation = Translation.LinearInterpolate(targetPos, delta * 10f);
+                  
+        Rotation = new Vector3(0, mechState.lookTarget.x, 0);
+        RotateObjectLocal(Vector3.Right, mechState.lookTarget.y);
+    }
+
+    public Vector3 AimPoint() {
+        if(!_camRaycast.IsColliding()) {
+            return ToGlobal(Translation + _camRaycast.CastTo);
+        }
+
+        return _camRaycast.GetCollisionPoint();
+    }
 }
---
+++ src/player/SauceArmControl.cs
@@ -0,0 +1,110 @@
+using Godot;
+using System;
+
+public class SauceArmControl : Spatial
+{
+    [Export]
+    public NodePath animationTreePath;
+    private AnimationTree _tree;
+
+    private AnimationNodeStateMachinePlayback _stateMachine;
+
+    private AnimationPlayer _animationPlayer;
+
+    [Export]
+    NodePath abilityControlPath;
+    private AbilityControl _abilityControl;
+
+    [Export]
+    PackedScene sauceGlobPrefab;
+
+    [Export]
+    NodePath cockpitControlPath;
+    private CockpitControl _cockpitControl;
+
+    [Export]
+    NodePath muzzleNodePath;
+    private Spatial _muzzle;
+
+    private Spatial _entityHolder;
+
+    private float _shotCooldown;
+
+    private float _fireDelay = 0.1f;
+
+    private bool _firing = false;
+
+    private float _muzzleVelocity = 50;
+
+    private Random _rng = new Random();
+
+    public override void _Ready()
+    {
+        _tree = GetNode<AnimationTree>(animationTreePath) ?? throw new NullReferenceException();
+        _stateMachine = (AnimationNodeStateMachinePlayback)_tree.Get("parameters/playback") ?? throw new NullReferenceException();
+        _animationPlayer = _tree.GetNode<AnimationPlayer>(_tree.AnimPlayer) ?? throw new NullReferenceException();
+        _abilityControl = GetNode<AbilityControl>(abilityControlPath) ?? throw new NullReferenceException();
+        _entityHolder = LevelManager.MustGetNode(this).EntityHolder ?? throw new NullReferenceException();
+        _cockpitControl = GetNode<CockpitControl>(cockpitControlPath) ?? throw new NullReferenceException();
+        _muzzle = GetNode<Spatial>(muzzleNodePath) ?? throw new NullReferenceException();
+
+        _abilityControl.Connect(nameof(AbilityControl.OnAbilityChange), this, nameof(_HandleAbilityChanged));
+        _abilityControl.Connect(nameof(AbilityControl.OnClick), this, nameof(_HandleClick));
+
+    }
+
+    private void _HandleAbilityChanged(MechAbility ability)
+    {
+        if (ability == MechAbility.Sauce)
+        {
+            _stateMachine.Travel("GunIdle");
+        }
+        else
+        {
+            _firing = false;
+            _stateMachine.Travel("GunStowed");
+        }
+    }
+
+    private void _HandleClick(bool isPressed, bool leftClick)
+    {
+        if (_abilityControl.AbilityState.activeAbility != MechAbility.Sauce)
+        {
+            return;
+        }
+
+        if(leftClick) {
+            _firing = isPressed;
+        }
+
+    }
+
+    public override void _Process(float delta)
+    {
+        if(_shotCooldown > 0) {
+            _shotCooldown-=delta;
+        }
+
+        if(!_firing){
+            return;
+        }
+
+        if(_shotCooldown <= 0) {
+            var glob = sauceGlobPrefab.Instance<SauceGlobControl>();
+            _entityHolder.AddChild(glob);
+
+    		glob.Translation = _muzzle.GlobalTranslation;
+
+            var aimPoint = _cockpitControl.AimPoint();
+            var aimDirection = (aimPoint - _muzzle.GlobalTranslation).Normalized();
+
+            glob.GlobalTransform = new Transform(GlobalTransform.basis, glob.Transform.origin);
+            glob.Rotate(glob.Transform.basis.z, Mathf.Lerp(-Mathf.Pi, Mathf.Pi, (float)_rng.NextDouble()));
+            glob.Rotate(glob.Transform.basis.x, Mathf.Pi / 2f);
+
+            glob.LinearVelocity = aimDirection * _muzzleVelocity;
+
+            _shotCooldown = _fireDelay;
+        }
+    }
+}
